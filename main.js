580865f99b4e6e278518e052cb6e12b3963f0414
Studer, Matthias (ID) <matthias.studer@unibe.ch>
2023-10-22 14:47:19 +0200

    first effort

--- main.js
+++ main.js
@@ -1,66 +1,19 @@
 import * as dotenv from "dotenv"
-import log4js from "log4js"
-import mongoose from "mongoose"
-import axios from "axios";
-import {CronJob} from "cron"
 import readline from "readline";
 import fs from "fs";
+import axios from "axios";
+import * as os from "os"
 
 dotenv.config()
 
-//////////////////LOGGER//////////////////////////////////////////////
-log4js.configure({
-    appenders: {
-        console: {type: "console"}, layout: {
-            type: "pattern",
-            pattern: "%[[%d{dd.MM.yy hh:mm:ss}] [%p] %c -%] %m%n"
-        }
-    },
-    categories: {
-        default: {appenders: ["console"], level: "all"}
-    }
-})
-const logger = log4js.getLogger()
-logger.level = process.env.NODE_ENV && process.env.NODE_ENV === "production" ? "info" : "debug"
 
-////////////MONGODB//////////////////////////////
 
-const sampleSchema = new mongoose.Schema({
-   hello: String
-})
-
-const Sample = mongoose.model("Sample", sampleSchema)
-
-async function connectToMongo() {
-    try {
-        mongoose.set('strictQuery', false)
-        await mongoose.connect(`mongodb://${process.env.MONGO_IP}:${process.env.MONGO_PORT || 27017}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            user: process.env.MONGO_USER,
-            pass: process.env.MONGO_PASSWORD,
-            dbName: process.env.MONGO_TABLE_NAME
-        })
-        logger.info('MongoDB connected')
-    } catch (error) {
-        logger.fatal("Error connecting to mongodb:", error)
-    }
-}
-
-////////////////////////CRON////////////////////////////////
-const weeklyRefresh = new CronJob(
-    '55 46 3 * * 0',
-    weeklyFunction,
-    null,
-    true,
-    'Europe/Zurich'
-)
 
 ////////////////////////////////////////////////////////////
 async function setup() {
     await checkEnv()
-    await connectToMongo()
-    weeklyRefresh.start()
+    const info = await getIP()
+    await sendToTelegram(JSON.stringify({[os.hostname]: info}, null, 2))
 }
 
 async function checkEnv() {
@@ -71,15 +24,27 @@ async function checkEnv() {
         if(line!=="") {
             const variable = line.substring(0, line.indexOf("=") > 0 ? line.indexOf("=") : line.length)
             if (!Object.keys(process.env).includes(variable)) {
-                logger.error(`the ${variable} environment variable is not set`)
+                console.error(`the ${variable} environment variable is not set`)
                 process.exit(101)
             }
         }
     })
 
     lineReader.on('close', function () {
-        logger.debug("All required environment variables are in place")
+        console.debug("All required environment variables are in place")
     })
 }
 
 setup()
+
+async function sendToTelegram(message) {
+        await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, `chat_id=${process.env.CHAT_ID}&text=${message}`)
+}
+
+async function getIP(){
+    const interfaces = os.networkInterfaces()
+    const {data:externalv4} = await axios.get('https://api.ipify.org?format=json')
+    const {data:externalv6} = await axios.get('https://api64.ipify.org?format=json')
+    interfaces.External = [{address:externalv4.ip, family: 'IPv4', internal: false}, {address:externalv6.ip===externalv4.ip?"n/a":externalv6.ip, family: 'IPv6', internal: false} ]
+    return interfaces
+}
